2edfbd507fd8bf934fdda213a64afa526ab806c3
bigcoz <119919826+bigcoz@users.noreply.github.com>
2023-11-23 14:42:13 +0900

    Update timetable.js

--- timetable.js
+++ timetable.js
@@ -1,34 +1,86 @@
+function handleCellClick(cell) {
+  // Set the currentCell variable to keep track of the clicked cell
+  currentCell = cell;
 
-    document.addEventListener('DOMContentLoaded', function() {
-      // Retrieve values from local storage on page load
-      loadTableValues();
+  // Load the previously selected value from local storage
+  var storedValue = localStorage.getItem('tableData') ? JSON.parse(localStorage.getItem('tableData'))[currentCell.id] : null;
+  if (storedValue) {
+    document.getElementById('modalInput').value = storedValue;
+  }
 
-      // Add event listeners to table cells
-      const table = document.getElementById('locationTable');
-      table.addEventListener('input', function(event) {
-        const cell = event.target;
-        const row = cell.parentElement.rowIndex;
-        const col = cell.cellIndex;
-        const value = cell.textContent.trim();
+  // Display the modal
+  document.getElementById('myModal').style.display = 'block';
+}
 
-        // Update local storage with the new value
-        updateLocalStorage(row, col, value);
-      });
+// Function to save the selected value to the table cell and local storage
+function saveSelectedValue() {
+  var modalInput = document.getElementById('modalInput');
+  var selectedValue = modalInput.value;
+
+  // Check if a cell was clicked
+  if (currentCell) {
+    // Save the selected value to the table cell
+    currentCell.textContent = selectedValue;
+
+    // Save the entire table data to local storage
+    saveTableData();
+
+    // Close the modal
+    document.getElementById('myModal').style.display = 'none';
+  }
+}
+
+window.addEventListener('load', function() {
+  if (typeof(Storage) !== "undefined") {
+    // Load the entire table data from local storage
+    loadTableData();
+  } else {
+    console.error("Local storage is not supported");
+  }
+});
+
+var cells = document.querySelectorAll('#locationTable tbody td');
+cells.forEach(function(cell) {
+  cell.addEventListener('click', function() {
+    handleCellClick(cell);
+  });
+});
+
+// Variable to keep track of the clicked cell
+var currentCell;
+
+// Function to save the entire table data to local storage
+function saveTableData() {
+  const tableData = [];
+
+  const rows = document.querySelectorAll("#locationTable tbody tr");
+  rows.forEach((row) => {
+    const rowData = [];
+
+    const cells = row.querySelectorAll("td");
+    cells.forEach((cell) => {
+      rowData.push(cell.textContent);
     });
 
-    // Function to load table values from local storage
-    function loadTableValues() {
-      for (let row = 1; row < 17; row++) {
-        for (let col = 1; col < 8; col++) {
-          const cellValue = localStorage.getItem(`cell_${row}_${col}`);
-          if (cellValue !== null) {
-            document.getElementById('locationTable').rows[row].cells[col].textContent = cellValue;
-          }
-        }
-      }
-    }
+    tableData.push(rowData);
+  });
 
-    // Function to update local storage with a new cell value
-    function updateLocalStorage(row, col, value) {
-      localStorage.setItem(`cell_${row}_${col}`, value);
-    }
+  localStorage.setItem("tableData", JSON.stringify(tableData));
+}
+
+// Function to load the entire table data from local storage
+function loadTableData() {
+  const storedData = localStorage.getItem("tableData");
+
+  if (storedData) {
+    const tableData = JSON.parse(storedData);
+
+    const rows = document.querySelectorAll("#locationTable tbody tr");
+    rows.forEach((row, rowIndex) => {
+      const cells = row.querySelectorAll("td");
+      cells.forEach((cell, cellIndex) => {
+        cell.textContent = tableData[rowIndex][cellIndex];
+      });
+    });
+  }
+}
